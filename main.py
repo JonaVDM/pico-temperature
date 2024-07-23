795c372bb1a874e05f4d15398f87dda2f2cffa77
Jonathan <jonajvdm@gmail.com>
2024-07-23 21:16:53 +0200

    rewrote with functions

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
 import dht
 from machine import Pin
-from utime import sleep
+from utime import sleep, time
 import config
 import ujson
 import network
 import urequests
-import utime as time
-
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(config.SSID, config.PASSWORD)
 
 sensor = dht.DHT22(Pin(15))
 last_update = 0
 led = Pin(config.PIN_LED, Pin.OUT)
 
-while True:
-    if last_update + config.TIMEOUT > time.time():
-        continue
-    last_update = time.time()
 
-    try:
-        sensor.measure()
-        temp = sensor.temperature()
-        hum = sensor.humidity()
+def main():
+    connect_to_network()
 
-        led.on()
-        data = ujson.dumps({'temperature': temp, 'humidity': hum})
-        res = urequests.post(config.URL, headers = {'Content-Type': 'application/json'}, data = data)
-        print(res)
-        led.off()
-
-        print('Temperature: %3.1f C' % temp)
-        print('Humidity: %3.1f %%' % hum)
+    last_update = 0
+    while True:
+        if last_update + config.TIMEOUT > time():
+            continue
+        last_update = time()
+        try:
+            temp, hum = measure()
+            upload(temp, hum)
+        except Exception as e:
+            print(f'[main] error caught: {e}')
         sleep(1)
-    except OSError as e:
-        led.on()
-        print('Failed to read sensor.', e)
+
+
+def connect_to_network():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(config.SSID, config.PASSWORD)
+    while not wlan.isconnected():
+        print(f'[network] waiting for network connection, status: {wlan.status()}')
+        sleep(1)
+    print(f'[network] connected to the network: {wlan.ifconfig()}')
+
+
+def measure():
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    print(f'[measure] temp = {temp}; hum = {hum}')
+    return temp, hum
+
+
+def upload(temperature, humidity):
+    led.on()
+    data = ujson.dumps({
+        'temperature': temperature,
+        'humidity': humidity,
+    })
+    try:
+        res = urequests.post(config.URL, headers={'Content-Type': 'application/json'}, data=data)
+        print(f'[sender] request send; status: {res.status_code}')
+        led.off()
+    except Exception as e:
+        print(f'[sender] error {e}')
+
+
+main()
